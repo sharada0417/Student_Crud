1c7874fe3509488f8e58a4cc532ce372a67547b7
sharada417 <sharada.kanchana2001@gmail.com>
2025-05-23 23:52:27 +0530

    create Crud controller

--- SecondChance/src/main/java/com/example/demo/controller/CourseController.java
+++ SecondChance/src/main/java/com/example/demo/controller/CourseController.java
@@ -1,5 +1,20 @@
 package com.example.demo.controller;
 
-public interface CourseController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.model.Course;
+
+@RestController
+@RequestMapping("/course")
+public interface CourseController extends CRUDController <String,Course>{
+	public CourseController() {
+		Course ecommerce = new Course("IT3232","Practical for e commerce",2);
+		Course webservice = new Course("IT2234","Web service and swerver Techonolgy",4);
+		Course webapplication = new Course("CSC3132","WebService Apllication development",2);
+		
+		getMap().put(webapplication.getCode(),webapplication);
+		getMap().put(webservice.getCode(),webservice);
+		getMap().put(ecommerce.getCode(),ecommerce);
+	}
 }
