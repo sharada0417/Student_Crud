11cf5690ba61b88421cb29887c4eaeae319974f4
sharada417 <sharada.kanchana2001@gmail.com>
2025-05-24 00:27:09 +0530

    fix errors

--- SecondChance/src/main/java/com/example/demo/controller/CRUDController.java
+++ SecondChance/src/main/java/com/example/demo/controller/CRUDController.java
@@ -1,5 +1,59 @@
 package com.example.demo.controller;
 
-public class CRUDController {
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class CRUDController<K,T>{
+private Map<K,T> objects = new HashMap<K,T>();
+	
+	public Map<K,T> getMap(){
+		return objects;
+	}
+	
+	@GetMapping("/")
+	public Map<K,T> getAll(){
+		return objects;
+	}
+	
+	@GetMapping("/{id}")
+	public T get(@PathVariable("id") K id){
+		return objects.get(id);
+	}
+	
+	 @PostMapping("/add")
+	    public T addNew(@RequestBody T object, @RequestParam("id") K id) {
+	        objects.put(id, object);
+	        return object; // Return the newly added object
+	    }
+	
+	@PutMapping("/update/{id}")
+	public String updateOne(@PathVariable("id") K id, @RequestBody T objectNew) {
+	    if (objects.get(id) != null) {
+	        objects.put(id, objectNew);
+	        return "Updated object details.";
+	    }
+	    return "404 couldn't find the object.";
+	}
+		
+	//delete
+		@DeleteMapping("/delete/{id}")
+		public String deleteOne(@PathVariable("id") K id)
+		{
+			if(objects.get(id) != null)
+			{
+				objects.remove(id);
+				return "The Object Deleted Sucessfully.";
+			}
+		
+		return "404 couldn't find the student";
+		}
+	
 }
--- SecondChance/src/main/java/com/example/demo/controller/CourseController.java
+++ SecondChance/src/main/java/com/example/demo/controller/CourseController.java
@@ -7,14 +7,20 @@ import com.example.demo.model.Course;
 
 @RestController
 @RequestMapping("/course")
-public interface CourseController extends CRUDController <String,Course>{
-	public CourseController() {
-		Course ecommerce = new Course("IT3232","Practical for e commerce",2);
-		Course webservice = new Course("IT2234","Web service and swerver Techonolgy",4);
-		Course webapplication = new Course("CSC3132","WebService Apllication development",2);
+public class CourseController extends CRUDController<String,Course>{
+	
+	public CourseController ()
+	{
+		Course ecommerce = new Course("IT3232","Practical for E-commerce",2);
+		Course webservice = new Course("IT2234","WebService and server Technology",4);
+		Course webapplication = new Course("CSC3132","Web Application Development",2);
 		
 		getMap().put(webapplication.getCode(),webapplication);
 		getMap().put(webservice.getCode(),webservice);
 		getMap().put(ecommerce.getCode(),ecommerce);
+		
+		
+		
+		
 	}
 }
